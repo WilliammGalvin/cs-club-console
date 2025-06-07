45c34cfe4f7a4c81485c0831748727e9b325b857
Liam <79233903+WilliammGalvin@users.noreply.github.com>
2025-06-06 22:46:16 -0400

    Update console_err.js

--- console_err.js
+++ console_err.js
@@ -1,13 +1,14 @@
 export class ConsoleError extends Error {
   constructor(msg) {
     super(msg);
-    this.name = "ConsoleError";
+    this.name = "Error";
   }
 }
 
 export class InternalConsoleError extends Error {
   constructor(context, msg) {
-    super(context + ": " + msg);
-    this.name = "InternalConsoleError";
+    super("Internal console error.");
+    this.name = "";
+    console.error(context + ": " + msg);
   }
 }
