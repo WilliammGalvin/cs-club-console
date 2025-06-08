6d0facc2599866242bf0fdfbaacbd2e521d09f73
Liam <79233903+WilliammGalvin@users.noreply.github.com>
2025-06-08 17:42:17 -0400

    updated func find logic

--- commands.js
+++ commands.js
@@ -2,66 +2,63 @@ import CmdSet from "./cmd_set.json" assert { type: "json" };
 import { ConsoleError, InternalConsoleError } from "./console_err.js";
 
 /* Add commands here */
-const printSay = (cmd_obj, printConsole) => {
-  const { flags, args } = cmd_obj;
-  let content = args.content;
-  let times = args?.times ?? 1;
+const commands = {
+  say: (cmd_obj, printConsole) => {
+    const { flags, args } = cmd_obj;
+    let content = args.content;
+    let times = args?.times ?? 1;
 
-  if (flags.includes("scream")) {
-    content = content.toUpperCase();
-  } else if (flags.includes("whisper")) {
-    content = content.toLowerCase();
-  }
-
-  for (let i = 0; i < times; i++) {
-    printConsole(content ?? "");
-  }
-};
-
-const helpCmd = (cmd_obj, printConsole) => {
-  if (Object.keys(cmd_obj.args).length > 0) {
-    const target = getCmdInfoObj().find((obj) => obj.cmd == cmd_obj.args.name);
-
-    if (!target) {
-      throw new ConsoleError("Invalid command argument.");
+    if (flags.includes("scream")) {
+      content = content.toUpperCase();
+    } else if (flags.includes("whisper")) {
+      content = content.toLowerCase();
     }
 
-    if (!target.is_secret) {
-      if (target.is_secret) {
+    for (let i = 0; i < times; i++) {
+      printConsole(content ?? "");
+    }
+  },
+
+  help: (cmd_obj, printConsole) => {
+    if (Object.keys(cmd_obj.args).length > 0) {
+      const target = getCmdInfoObj().find(
+        (obj) => obj.cmd == cmd_obj.args.name
+      );
+
+      if (!target) {
         throw new ConsoleError("Invalid command argument.");
       }
+
+      if (!target.is_secret) {
+        if (target.is_secret) {
+          throw new ConsoleError("Invalid command argument.");
+        }
+      }
+
+      printConsole("Name:\n  " + target.cmd + "\n");
+      printConsole("Description:\n  " + target.description + "\n");
+      printConsole("Usage:\n  " + target.usage + "\n");
+      return;
     }
 
-    printConsole("Name:\n  " + target.cmd + "\n");
-    printConsole("Description:\n  " + target.description + "\n");
-    printConsole("Usage:\n  " + target.usage + "\n");
-    return;
-  }
+    printConsole("Available Commands:");
+    const cmds = getCmdInfoObj().filter((obj) => !(obj.is_secret ?? false));
+    const maxCmdLength = Math.max(...cmds.map((obj) => obj.cmd.length));
 
-  printConsole("Available Commands:");
-  const cmds = getCmdInfoObj().filter((obj) => !(obj.is_secret ?? false));
-  const maxCmdLength = Math.max(...cmds.map((obj) => obj.cmd.length));
+    cmds.forEach((obj) => {
+      if (obj.is_secret ?? false) return;
+      printConsole("  " + obj.cmd.padEnd(maxCmdLength + 3) + obj.usage);
+    });
 
-  cmds.forEach((obj) => {
-    if (obj.is_secret ?? false) return;
-    printConsole("  " + obj.cmd.padEnd(maxCmdLength + 3) + obj.usage);
-  });
+    printConsole(
+      "\n\nType 'help <command>' for usage, examples, and option details."
+    );
+  },
 
-  printConsole(
-    "\n\nType 'help <command>' for usage, examples, and option details."
-  );
+  who: (cmd_obj, printConsole) => {
+    printConsole("Who, me?");
+  },
 };
-
-const whoCmd = (cmd_obj, printConsole) => {
-  printConsole("Who, me?");
-};
-
-/* -- */
-const command_set = new Map([
-  ["say", printSay],
-  ["help", helpCmd],
-  ["who", whoCmd],
-]);
 
 export const getCmdInfoObj = () => {
   if (!CmdSet) {
@@ -72,14 +69,14 @@ export const getCmdInfoObj = () => {
 };
 
 const findAndRunCmd = (cmd, printConsole) => {
-  if (!command_set.has(cmd.id)) {
-    throw new InternalConsoleError(
-      "Commands",
-      "Command '" + cmd.id + "' not found."
-    );
+  const cmd_func = commands[cmd.id];
+
+  if (typeof cmd_func == "function") {
+    cmd_func(cmd, printConsole);
+    return;
   }
 
-  command_set.get(cmd.id)(cmd, printConsole);
+  throw ConsoleError("Unknown command.");
 };
 
 export default findAndRunCmd;
